f71b291f6eeab289b23b339ce66a668d6be1864b
DakayTai <gakpenting0102@gmail.com>
2024-10-23 13:20:33 +0700

    Update revip.py

--- revip.py
+++ revip.py
@@ -1,31 +1,35 @@
-#!/usr/bin/env python2
-# Author: Mr. BDKR28
+#!/usr/bin/env python3
+# Author: Maxim3lian
 
 import requests
 import os
-import time
 import re
 from multiprocessing.dummy import Pool as ThreadPool
-from colorama import Fore, Style, init
+from colorama import init
 
 init(autoreset=True)
+
+# Using a set to store unique results
+unique_urls = set()
 
 def ask_dns(url):
     try:
         headers = {
             'User-Agent': 'Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36'
         }
-        x = requests.get('https://askdns.com/ip/'+url, headers=headers, timeout=30).content
+        response = requests.get(f'https://askdns.com/ip/{url}', headers=headers, timeout=30)
+        x = response.text  # .text gives string content in Python 3
         if 'Domain Name' in x:
             regex = re.findall('<a href="/domain/(.*?)">', x)
             for domain_name in regex:
                 website_url = 'http://' + domain_name
-                print("GRABBED: {}".format(website_url))
-                open('Reversed.txt', 'a').write(website_url + '\n')
+                if website_url not in unique_urls:  # Check for duplicates
+                    print(f"GRABBED: {website_url}")
+                    unique_urls.add(website_url)  # Add to the set
         else:
-            print("BAD : " + url)
-    except:
-        pass
+            print(f"BAD : {url}")
+    except Exception as e:
+        print(e)
 
 
 def rapid_dns(url):
@@ -33,38 +37,47 @@ def rapid_dns(url):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36'
         }
-        x = requests.get('https://rapiddns.io/s/'+url+'?full=1&down=1#result/', headers=headers, timeout=30).content
+        response = requests.get(f'https://rapiddns.io/s/{url}?full=1&down=1#result/', headers=headers, timeout=30)
+        x = response.text  # .text gives string content in Python 3
         if '<th scope="row ">' in x:
             regex = re.findall('<td>(?!\-)(?:[a-zA-Z\d\-]{0,62}[a-zA-Z\d]\.){1,126}(?!\d+)[a-zA-Z]{1,63}</td>', x)
             for domain_name in regex:
                 website_url = 'http://' + domain_name.replace('<td>', '').replace('</td>', '').replace('ftp.', '').replace('images.', '').replace('cpanel.', '').replace('cpcalendars.', '').replace('cpcontacts.', '').replace('webmail.', '').replace('webdisk.', '').replace('hostmaster.', '').replace('mail.', '').replace('ns1.', '').replace('ns2.', '').replace('autodiscover.', '')
-                print("GRABBED: {}".format(website_url))
-                open('reversed.txt', 'a').write(website_url + '\n')
-                print("Save reversed.txt")
+                if website_url not in unique_urls:  # Check for duplicates
+                    print(f"GRABBED: {website_url}")
+                    unique_urls.add(website_url)  # Add to the set
         else:
-            print("BAD : " + url)
-    except:
-        pass
+            print(f"BAD : {url}")
+    except Exception as e:
+        print(e)
 
 
 def reverse_ip_lookup(url):
     try:
         rapid_dns(url)
         ask_dns(url)
-    except:
-        pass
+    except Exception as e:
+        print(e)
 
 
 def main():
     os.system('cls' if os.name == 'nt' else 'clear')
     try:
-        ip_list = raw_input("IPS LIST : ")
-        urls = open(ip_list, 'r').read().splitlines()
-        num_threads = raw_input("THREAD : ")
+        ip_list = input("File Ip : ")  # Use input() for Python 3
+        with open(ip_list, 'r') as f:
+            urls = f.read().splitlines()
+        num_threads = input("Threads : ")  # Use input() for Python 3
+        with open('Reversed.txt', 'w') as f:
+            for url in unique_urls:
+                f.write(url + '\n')
         pool = ThreadPool(int(num_threads))
         pool.map(reverse_ip_lookup, urls)
-    except:
-        pass
+        
+        # Write unique URLs to Reversed.txt
+        print("Results saved to Reversed.txt")
+        
+    except Exception as e:
+        print(e)
 
 
 if __name__ == '__main__':
